a99db6f455528c81e75a87e215119206a43905c1
daniel-kav <dankavatha01@gmail.com>
2024-12-25 20:47:41 +0300

    home page boilercode

---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Section from '../components/Section';
+
+const Home: React.FC = () => <Section id="home" bgColor="#f5f5f5" title="Empowering Lives with Quality Supplies" />;
+
+export default Home;
