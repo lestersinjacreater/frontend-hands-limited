2080a141e93ecf739b0be83d3a1882afced781b2
daniel-kav <dankavatha01@gmail.com>
2024-12-26 22:19:47 +0300

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+    return (
+      <footer className="bg-blue-600 text-white py-8">
+        <div className="container mx-auto text-center">
+          <p>&copy; 2023 Quality Supplies Company. All rights reserved.</p>
+        </div>
+      </footer>
+    );
+  }
+  
+  
