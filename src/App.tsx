3e93cd7faa32b5fbe8dd1056f32be0d997a438ec
daniel-kav <dankavatha01@gmail.com>
2024-12-27 20:03:45 +0300

    children imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { Products } from './components/Products';
+import { Services } from './components/Services';
+import { Updates } from './components/Updates';
+import { Testimonials } from './components/Testimonials';
+import { WhyChooseUs } from './components/WhyChooseUs';
+import { AboutUs } from './components/AboutUs';
+import { ContactForm } from './components/ContactForm';
+import { Footer } from './components/Footer';
 
-const App = () => {
+export default function App() {
   return (
-      <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div aria-hidden="true" className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl">
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-          />
-      
-        </div>
-        <div className="mx-auto max-w-4xl text-center">
-          <h2 className="text-base/7 font-semibold text-indigo-600">Pricing</h2>
-          <p className="mt-2 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-6xl">
-            Choose the right plan for you
-          </p>
-      </div>
-      </div>
-  )
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main>
+        <Hero />
+        <Products title="PRODUCTS" />
+        <Services title="SERVICES" />
+        <Updates title="Updates" />
+        <Testimonials title="What Our Clients Say About Us" />
+        <WhyChooseUs title="Why Choose Us?" />
+        <AboutUs title="Who We Are" />
+        <ContactForm title="Contact Us" />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
