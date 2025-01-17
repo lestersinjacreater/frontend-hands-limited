0d6d5271db454bcd274c9bd069ee21217772d826
daniel-kav <dankavatha01@gmail.com>
2025-01-17 21:31:07 +0300

    refactor App component to use section tags for better structure and accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
-import { Header } from './components/Header';
-// import { Footer } from './components/Footer';
-import HeroSection from './components/Herosection';
-import Products from './components/Products';
-import TestimonialsScroll from './components/Testimonials';
-import UpdatesSection from './components/Updates';
-import WhoWeAreSection from './components/AboutUs';
-import ContactForm from './components/ContactForm';
-import WhyChooseUs from './components/WhyChooseUs';
-import Services from './components/Services';
+import WhoWeAreSection from "./components/AboutUs";
+import ContactForm from "./components/ContactForm";
+import { Header } from "./components/Header";
+import HeroSection from "./components/Herosection";
+import Products from "./components/Products";
+import Services from "./components/Services";
+import TestimonialsScroll from "./components/Testimonials";
+import UpdatesSection from "./components/Updates";
 
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main>
-        <HeroSection />
-        <Products />
-        <Services />
-        <TestimonialsScroll />
-        <UpdatesSection />
-        < WhyChooseUs />
-        <WhoWeAreSection />
-        <ContactForm />
+        <section id="home"><HeroSection /></section>
+        <section id="products"><Products /></section>
+        <section id="services"><Services /></section>
+        <section id="testimonials"><TestimonialsScroll /></section>
+        <section id="updates"><UpdatesSection /></section>
+        <section id="about"><WhoWeAreSection /></section>
+        <section id="contact"><ContactForm /></section>
       </main>
     </div>
   );
 }
-
