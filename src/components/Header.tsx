6473abfff018b73bc94ed591a0c835e6aeda7fe7
lestersinjacreater <lestetsinjawatosia@gmail.com>
2025-01-17 22:44:51 +0300

    fixing deployment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import  { useState, useEffect } from 'react'
+import {  useLocation } from 'react-router-dom'
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
