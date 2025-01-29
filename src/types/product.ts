f5ef862edf2e3bd3000cd69317644bfe245616e2
daniel-kav <dankavatha01@gmail.com>
2025-01-29 10:52:18 +0300

    add Product and ProductFormData interfaces for product type definitions

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    productid: number;
+    title: string;
+    description: string;
+    createdAt: string;
+  }
+  
+  export interface ProductFormData {
+    title: string;
+    description: string;
+  }
